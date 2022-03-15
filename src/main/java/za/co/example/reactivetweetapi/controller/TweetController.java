1af6fb8e568a976fc5d01afed713789aea795a65
tkalam <toufeeq.kalam@metropolitan.co.za>
2022-03-15 17:03:10 +0200

    added swagger-ui

--- src/main/java/za/co/example/reactivetweetapi/controller/TweetController.java
+++ src/main/java/za/co/example/reactivetweetapi/controller/TweetController.java
@@ -3,10 +3,12 @@ package za.co.example.reactivetweetapi.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 import za.co.example.reactivetweetapi.model.Tweet;
 import za.co.example.reactivetweetapi.repository.TweetRepository;
@@ -21,14 +23,26 @@ public class TweetController {
     @Autowired
     TweetRepository tweetRepository;
 
-    @PostMapping(value = "/tweet", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<?>> saveTweet(@RequestBody @Valid Tweet tweet) {
-        return this.tweetRepository.save(tweet).map(response -> new ResponseEntity<>(tweet, HttpStatus.OK));
+    @GetMapping(value = "/tweet/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<?>> getTweetById(@PathVariable String id) {
+        return this.tweetRepository.findById(id).flatMap(tweet -> Mono.just(new ResponseEntity<>(tweet, HttpStatus.OK)));
     }
 
-    @GetMapping(value = "/tweet/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<Tweet>> getTweetById(@PathVariable String id) {
-        return this.tweetRepository.findById(id).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<?>> getAllTweets() {
+        return this.tweetRepository.findAll(Sort.by(Sort.Direction.DESC, "created")).collectList().flatMap(tweets -> {
+            if (tweets.isEmpty()) {
+                return Mono.just(new ResponseEntity<>(tweets, HttpStatus.OK));
+            } else {
+                return Mono.just(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+            }
+        });
+    }
+
+    @PostMapping(value = "/tweet", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<Tweet>> saveTweet(@RequestBody @Valid Tweet tweet) {
+        return this.tweetRepository.save(tweet)
+                .map(response -> new ResponseEntity<>(tweet, HttpStatus.OK));
     }
 
     @PutMapping("/tweet/{id}")
@@ -44,9 +58,7 @@ public class TweetController {
     @DeleteMapping(value = "/tweet/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<ResponseEntity<Void>> deleteTweetById(@PathVariable String id) {
         return tweetRepository.findById(id).flatMap(existingTweet -> this.tweetRepository.delete(existingTweet)
-                        .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK))))
-                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND)));
     }
-
-
 }
--- src/main/java/za/co/example/reactivetweetapi/controller/exception/RestExceptionHandler.java
+++ src/main/java/za/co/example/reactivetweetapi/controller/exception/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package za.co.example.reactivetweetapi.controller.exception;
 
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,10 +19,13 @@ public class RestExceptionHandler {
     public ResponseEntity<?> handleException(WebExchangeBindException e) {
         BindingResult bindingResult = e.getBindingResult();
         List<ValidationError> validationErrors = new ArrayList<>();
-        bindingResult.getFieldErrors().forEach(fieldError -> {
-            validationErrors.add(new ValidationError(fieldError.getObjectName(), fieldError.getDefaultMessage()));
-        });
+        bindingResult.getFieldErrors().forEach(fieldError -> validationErrors.add(new ValidationError(fieldError.getObjectName(), fieldError.getDefaultMessage())));
         return ResponseEntity.badRequest().body(validationErrors);
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<?> handleDuplicateKeyException(DuplicateKeyException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
 }
